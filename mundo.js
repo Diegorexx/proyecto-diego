cea55b1cabbaa37d40732d248c6230c967a555c9
Diego Morales <diegorex964@gmail.com>
2026-03-09 20:04:13 -0600

    agregue diferentes estilos

---
+++ mundo.js
@@ -0,0 +1,10 @@
+
+const clickeame=()=>{
+   // colorDeTexto();
+     document.getElementById('primer_parrafo').style.color = "green";
+}
+
+const lineaColor=()=>{
+   // colorDeTexto();
+     document.getElementById('segundo_parrafo').style.background = "red";
+}
